e6d04ff0e3767b3b72356706269491cf200b144a
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-01-17 03:36:57 +0200

    FIle check

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,11 +8,10 @@ from sys import argv
 
 def number_of_subscribers(subreddit):
     """queries the Reddit API and returns the number of subscribers"""
-
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'user-agent': 'request'}
     res = requests.get(url, headers=headers,
-                            allow_redirects=False)
+                       allow_redirects=False)
     if str(res) != "<Response [200]>":
         return 0
     data = res.json()
