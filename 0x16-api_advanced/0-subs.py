e5fa55c5d5a1ac1c84c04010a33060b6ed161570
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-01-17 03:34:48 +0200

    FIle check

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 """
-Module for number of subscribers
+    Module for number of subscribers
 """
-
 import requests
+from sys import argv
 
 
 def number_of_subscribers(subreddit):
     """queries the Reddit API and returns the number of subscribers"""
 
-    if subreddit is None or type(subreddit) is not str:
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'user-agent': 'request'}
+    res = requests.get(url, headers=headers,
+                            allow_redirects=False)
+    if str(res) != "<Response [200]>":
         return 0
-
-    data = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-                                v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    num = data.get("data", {}).get("subscribers", 0)
+    data = res.json()
+    num = data.get("data").get("subscribers")
     return num
