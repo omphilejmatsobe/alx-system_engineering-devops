e0335690b7b17e2c755e3e4530b787417ce46c54
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-01-17 02:51:33 +0200

    File check

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,16 +7,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Function that queries the Reddit API and returns number of subscribers
-    and if not a valid subreddit, return 0.
-    """
-    data = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-    )
+    """queries the Reddit API and returns the number of subscribers"""
 
-    if data.status_code == 200:
-        return data.json().get("data").get("subscribers")
-    else:
+    if subreddit is None or type(subreddit) is not str:
         return 0
+
+    data = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+                                v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    num = data.get("data", {}).get("subscribers", 0)
+    return num
