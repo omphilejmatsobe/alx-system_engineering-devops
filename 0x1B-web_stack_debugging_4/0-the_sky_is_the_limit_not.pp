24451e3ce59148567a467bec228c1aaf1b9b1526
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-01-26 01:07:15 +0200

    Test

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,14 @@
-# increases the capability of nginx
+# Increase nginx capabilites
 
-# increase ULIMIT
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
+# Increase ULIMIT
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
 
-
-# restart
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+# Restart
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
